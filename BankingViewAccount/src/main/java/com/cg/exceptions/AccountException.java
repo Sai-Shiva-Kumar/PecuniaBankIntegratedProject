b8b1b8a4328f36c41743ba17586492218ba9f0dd
Shiva <*sais11604@gmail.com*>
2020-09-11 13:05:51 +0530

    Integrated Project

---
+++ BankingViewAccount/src/main/java/com/cg/exceptions/AccountException.java
@@ -0,0 +1,15 @@
+package com.cg.exceptions;
+
+@SuppressWarnings("serial")
+public class AccountException extends Exception{
+
+	public AccountException() {
+		super();
+	}
+
+	public AccountException(String message) {
+		super(message);
+	}
+	
+
+}
---
+++ BankingViewAccount/src/main/java/com/cg/exceptions/CustomerException.java
@@ -0,0 +1,15 @@
+package com.cg.exceptions;
+
+@SuppressWarnings("serial")
+public class CustomerException extends Exception {
+
+	public CustomerException() {
+		super();
+	}
+
+	public CustomerException(String message) {
+		super(message);
+	}
+
+	
+}
